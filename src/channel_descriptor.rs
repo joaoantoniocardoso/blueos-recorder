b33d442d69a29e5391b8f0f61c10127ede30d79f
João Antônio Cardoso <joao.maker@gmail.com>
2026-06-12 22:11:01 -0300

    src: channel_descriptor: Use const instead of static

--- src/channel_descriptor.rs
+++ src/channel_descriptor.rs
@@ -154,7 +154,7 @@ impl fmt::Display for MessageEncoding {
     }
 }
 
-static MSGS_DIR: include_dir::Dir = include_dir::include_dir!("src/external/zBlueberry/msgs");
+const MSGS_DIR: include_dir::Dir = include_dir::include_dir!("src/external/zBlueberry/msgs");
 
 #[instrument(skip_all)]
 fn load_cdr_schema(schema: &str, schema_path: Option<&PathBuf>) -> Result<String> {
